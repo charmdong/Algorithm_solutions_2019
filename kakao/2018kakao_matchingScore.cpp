e8b853210b1ba9b706d19290921498b4b7f5a9f3
donggunchung <donggun.chung@nts-corp.com>
2019-09-06 18:24:43 +0900

    [Prob type : Kakao]

---
+++ kakao/2018kakao_matchingScore.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string word, vector<string> pages);
+
+int main()
+{
+
+    return 0;
+}
+
+int solution(string word, vector<string> pages)
+{
+    
+}
